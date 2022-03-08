f19e46ff18e5d4d58810933c47bcc3670af890ff
Tadipartisahithi <003CHT744@IBM-PF3CYCFF>
2022-03-08 13:03:07 +0530

    Add project files.

---
+++ NullRef.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment1
+{
+    class NullRef
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            try
+            {
+                Console.WriteLine(str.Substring(5));
+            }
+            catch(NullReferenceException err)
+            {
+                Console.WriteLine("Please check the string str.");
+                Console.WriteLine(err.ToString());
+            }
+            Console.WriteLine("Continuing with other statments..");
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string PersonName = "Siva";
+                string PersonPlace = "Kurnool";
+                StringBuilder sBuilder = new StringBuilder(PersonName.Length, PersonName.Length);
+
+                sBuilder.Append(PersonName);
+                sBuilder.Insert(value: PersonPlace, index: PersonName.Length - 1, count: 1);
+            }
+            catch(System.OutOfMemoryException e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
